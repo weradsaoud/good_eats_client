8376254c7749f25e0f06df3f6d88b12613d46b53
weradsaoud <werad.saoud@gmail.com>
2022-06-14 16:15:03 +0300

    extras list

--- src/components/structures/itempage/Item.js
+++ src/components/structures/itempage/Item.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import { Carousel, CarouselItem, Checkbox, Radio, List, ListItem, Toolbar, Page } from 'react-onsenui';
+import { Carousel, CarouselItem, Checkbox, Radio, List, ListItem, Toolbar, Page, Button } from 'react-onsenui';
 import { useLocation } from 'react-router';
 import ItemToolbar from '../../views/itemtoolbar/itemToolbar';
 import './item.css';
 import OptionsToolbar from '../../views/optionstoolbar/OptionsToolbat';
 import * as actionsTypes from '../../../store/actions/actionsTypes';
 import { connect } from "react-redux";
+import { LinearProgress } from '@mui/material';
 
 function Item(props) {
 
     let options = [{ optionId: '1', name: 'option1', values: ['val1', 'val2', 'val3'] }, { optionId: '2', name: 'option2', values: ['val1', 'val2', 'val3'] }, { optionId: '3', name: 'option3', values: ['val1', 'val2', 'val3'] }, { optionId: '4', name: 'option4', values: ['val1', 'val2', 'val3'] }, { optionId: '5', name: 'option5', values: ['val1', 'val2', 'val3'] }, { optionId: '6', name: 'option6', values: ['val1', 'val2', 'val3'] }];
+    let extras = [{ extraId: '1', name: 'extra1', price: 10 }, { extraId: '2', name: 'extra2', price: 0.6 }, { extraId: '3', name: 'extra3', price: 0.5 }, { extraId: '4', name: 'extra4', price: 0.7 }]
 
 
     let [activeIndex, setActiveIndex] = useState(0);
@@ -24,7 +26,7 @@ function Item(props) {
             );
         })
     );
-
+    let [finishOptions, setFinishOptions] = useState(false);
     let optionsLength = options.length;
 
     let location = useLocation();
@@ -81,19 +83,29 @@ function Item(props) {
             <div></div>
         );
     };
-    const optionValueChecked = (val) => {
-        let optionsValuesCopy = [];
-        optionsValues.forEach((ov, idx) => {
-            if (ov.optionId == optionId) {
-                optionsValuesCopy.push({ ...ov, value: val });
-            } else {
-                optionsValuesCopy.push({ ...ov });
+    const optionValueChecked = (e, val) => {
+        if (!e.target.checked) {
+            e.preventDefault();
+        } else {
+            let optionsValuesCopy = [];
+            optionsValues.forEach((ov, idx) => {
+                if (ov.optionId == optionId) {
+                    optionsValuesCopy.push({ ...ov, value: val });
+                } else {
+                    optionsValuesCopy.push({ ...ov });
+                }
+            });
+            setOptionsValues(optionsValuesCopy);
+            if ((optionsLength > 0) && (activeIndex < (optionsLength - 1))) {
+                setActiveNext(true);
             }
-        });
-        setOptionsValues(optionsValuesCopy);
-        if ((optionsLength > 0) && (activeIndex < (optionsLength - 1))) {
-            setActiveNext(true);
+            if (activeIndex == (optionsLength - 1)) {
+                setFinishOptions(true);
+            }
         }
+    };
+    const extraSelected = (extra) => {
+        console.log('exra: ', extra);
     };
     let Carousels = options.map((o, idx) => {
         let caroselOptionVal = optionsValues.filter(ov => ov.optionId == o.optionId)[0];
@@ -104,14 +116,16 @@ function Item(props) {
                     renderHeader={renderValuesHeader}
                     renderRow={(val, idx) => {
                         return (
-                            <ListItem key={idx} tappable modifier=''>
+                            <ListItem key={idx} tappable modifier='' onClick={(e) => optionValueChecked(e, val)}>
                                 <div className='left'>
                                     <Checkbox
                                         onChange={(e) => optionValueChecked(val)}
                                         checked={val == caroselOptionVal.value}
                                         modifier='material' />
                                 </div>
-                                <div className='center'>{val}</div>
+                                <div className='center'>
+                                    <div className='option_value_div'>{val}</div>
+                                </div>
                                 <div className='right'></div>
                             </ListItem>
                         );
@@ -130,62 +144,61 @@ function Item(props) {
                 <div className='item_options_literal'>
                     Choose your options
                 </div>
-                <div className='item_options'>
-                    <OptionsToolbar
-                        optionName={optionName}
-                        activeNext={activeNext}
-                        activePrevious={activePrevious}
-                        previousOption={previousOption}
-                        nextOption={nextOption} />
-                    <Carousel
-                        onPostChange={() => console.log('onPostChange')}
-                        onOverscroll={() => console.log('onOverscroll')}
-                        onRefresh={() => console.log('onRefresh')}
-                        swipeable={false}
-                        overscrollable={false}
-                        autoScroll={false}
-                        fullscreen={false}
-                        autoScrollRatio={0.2}
-                        style={{ height: "100%" }}
-                        activeIndex={activeIndex}
-                    >
-                        {Carousels}
-                    </Carousel>
-
-
-                    {/* <List modifier={'noborder'}
-                        dataSource={}
-                        renderHeader={renderOptionsHeader}
-                        renderRow={(option, idx) => {
-                            return (
-                                <ListItem expandable tappable modifier='nodivider'>
-                                    <div className='left'>
-                                        <div className='option_name_div'>
-                                            {option.name}
-                                        </div>
-                                    </div>
-                                    <div className='expandable-content'>
-                                        <List modifier={'noborder'}
-                                            dataSource={option.values}
-                                            renderHeader={renderValuesHeader}
-                                            renderRow={(val, idx) => {
-                                                return (
-                                                    <ListItem tappable modifier=''>
-                                                        <div className='left'>
-                                                            <Checkbox
-                                                                onChange={() => optionValueChecked(val)}
-                                                                modifier='material' />
-                                                        </div>
-                                                        <div className='center'>{val}</div>
-                                                        <div className='right'></div>
-                                                    </ListItem>
-                                                );
-                                            }}
-                                        />
-                                    </div>
-                                </ListItem>
-                            );
-                        }} /> */}
+                <div className='options_extras_div'>
+                    <div className='item_options'>
+                        <OptionsToolbar
+                            optionName={optionName}
+                            activeNext={activeNext}
+                            activePrevious={activePrevious}
+                            previousOption={previousOption}
+                            nextOption={nextOption} />
+                        <Carousel
+                            onPostChange={() => console.log('onPostChange')}
+                            onOverscroll={() => console.log('onOverscroll')}
+                            onRefresh={() => console.log('onRefresh')}
+                            swipeable={false}
+                            overscrollable={false}
+                            autoScroll={false}
+                            fullscreen={false}
+                            autoScrollRatio={0.2}
+                            style={{ height: "100%" }}
+                            activeIndex={activeIndex}
+                        >
+                            {Carousels}
+                        </Carousel>
+                    </div>
+                    {/* <div className='extras_loader_progress'>
+                        <LinearProgress color="success" />
+                    </div> */}
+                    {finishOptions ? <div className='item_extras'>
+                        <div className='extras_literal'>
+                            Choose your extras
+                        </div>
+                        <div className='item_extras_list'>
+                            <List modifier={'noborder'}
+                                dataSource={extras}
+                                renderRow={(extra, idx) => {
+                                    return (
+                                        <ListItem tappable modifier='material'>
+                                            <div className='left'>
+                                                <Checkbox
+                                                    onChange={(e) => extraSelected(extra)}
+                                                    modifier='material' />
+                                            </div>
+                                            <div className='center'>
+                                                <div className='extra_value_div'>{extra.name}</div>
+                                            </div>
+                                            <div className='right'>
+                                                <div className='extra_price_div'> {extra.price}</div>
+                                            </div>
+                                        </ListItem>
+                                    );
+                                }} />
+                        </div>
+                    </div> : null}
+                    {finishOptions ? <div className='checkout_btn_div'>
+                        <Button className='checkout_btn'>Checkout</Button>
+                    </div> : null}
                 </div>
             </div>
         </div>
