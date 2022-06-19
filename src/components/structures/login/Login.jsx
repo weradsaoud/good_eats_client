7136587a733421a27cb79b77843284e3892dbfa9
weradsaoud <werad.saoud@gmail.com>
2022-06-19 16:28:16 +0300

    log in

--- src/components/structures/login/Login.jsx
+++ src/components/structures/login/Login.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import { firebase, auth } from '../../../globals/firebase';
+import SigninToolbar from '../../views/signintoolbar/SigninToolbar';
+import { Input, Button } from 'react-onsenui';
+import './login.css';
+import { useLocation, useNavigate } from 'react-router';
+import routes from '../../../globals/routes';
 
 const Login = () => {
 	// Inputs
@@ -7,6 +12,11 @@ const Login = () => {
 	const [otp, setotp] = useState('');
 	const [show, setshow] = useState(false);
 	const [final, setfinal] = useState('');
+
+
+	let location = useLocation();
+	let from = location.state;
+	let navigate = useNavigate();
 
 	// Sent OTP
 	const signin = () => {
@@ -16,11 +26,11 @@ const Login = () => {
 		let verify = new firebase.auth.RecaptchaVerifier('recaptcha-container');
 		auth.signInWithPhoneNumber(mynumber, verify).then((result) => {
 			setfinal(result);
-			//alert("code sent")
+			alert("code sent")
 			setshow(true);
 		})
 			.catch((err) => {
-				//alert(err);
+				alert(err);
 				window.location.reload()
 			});
 	}
@@ -30,33 +40,47 @@ const Login = () => {
 		if (otp === null || final === null)
 			return;
 		final.confirm(otp).then((result) => {
-			//alert('success')
+			navigate(routes.storesPageUrl);
+			alert('success');
 			console.log('result: ', result);
 			console.log('auth.currentUser: ', auth.currentUser);
 		}).catch((err) => {
-			//alert("Wrong code");
+			alert("Wrong code");
 		})
 	}
 
 	return (
-		<div style={{ "marginTop": "200px" }}>
-			<center>
-				<div style={{ display: !show ? "block" : "none" }}>
-					<input value={mynumber} onChange={(e) => {
-						setnumber(e.target.value)
-					}}
-						placeholder="phone number" />
-					<br /><br />
-					<div id="recaptcha-container"></div>
-					<button onClick={signin}>Send OTP</button>
+		<div className='login_container'>
+			<SigninToolbar></SigninToolbar>
+			<div className='login_content'>
+				<div className='phone_div' style={{ display: !show ? "block" : "none" }}>
+					<div className='phone_input_div'>
+						<Input
+							value={mynumber} float
+							onChange={(e) => {
+								setnumber(e.target.value)
+							}}
+							modifier='material'
+							placeholder='phone number' />
+					</div>
+					<div id="recaptcha-container" className='recaptcha_div'></div>
+					<div className='signin_btns_div'>
+						<Button onClick={signin} style={{'textTransform': 'none', 'width':'100%', 'background': 'rgba(31, 173, 62, 0.8)'}}>Login</Button>
+					</div>
 				</div>
-				<div style={{ display: show ? "block" : "none" }}>
-					<input type="text" placeholder={"Enter your OTP"}
-						onChange={(e) => { setotp(e.target.value) }}></input>
-					<br /><br />
-					<button onClick={ValidateOtp}>Verify</button>
+				<div className='verification_div' style={{ display: show ? "block" : "none" }}>
+					<div className='verify_input_div'>
+						<Input
+							value={mynumber} float
+							onChange={(e) => { setotp(e.target.value) }}
+							modifier='material'
+							placeholder='Enter your OTP' />
+					</div>
+					<div className='verify_btns_div'>
+						<Button onClick={ValidateOtp} style={{'textTransform': 'none', 'width':'100%', 'background': 'rgba(31, 173, 62, 0.8)'}}>Verify</Button>
+					</div>
 				</div>
-			</center>
+			</div>
 		</div>
 	);
 }
---
+++ src/components/views/signintoolbar/SigninToolbar.tsx
@@ -0,0 +1,63 @@
+import { KeyboardBackspace, ShoppingBasket } from "@mui/icons-material";
+import { Badge, IconButton } from "@mui/material";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router";
+import routes from "../../../globals/routes";
+import '../baskettoolbar/baskettoolbar.css';
+
+function SigninToolbar(props: any) {
+
+    let navigate = useNavigate();
+
+
+    const goToStore = () => {
+        let navigateState: any = {};
+        if(props.basket.length > 0){
+            navigateState = props.basket[0].store;
+        }else{
+            navigateState = props.selectedStore;
+        }
+        navigate(routes.storePageUrl, { state: navigateState });
+    };
+
+    let StoreName = (props.basket.length > 0) ? props.basket[0].store.name : 'Basket is empty'
+
+    return (
+        <div className="basket_topbar">
+            <div className="basket_topbarWrapper">
+                <div className="basket_topLeft">
+                    <IconButton size="small" id='prevBtn' className='basket_nave_icon' onClick={goToStore} aria-label="delete">
+                        <KeyboardBackspace />
+                    </IconButton>
+                </div>
+                <div className="basket_topMiddle">
+                    Login
+                </div>
+                <div className="basket_topRight">
+                    <div className='right'>
+                        {/* <IconButton onClick={props.toggleSideMenu} className='basket_nave_icon' aria-label="delete">
+                            <Badge badgeContent={props.basket.length} color='error'>
+                                <ShoppingBasket color="action" />
+                            </Badge>
+                        </IconButton> */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+const mapStateToProps = (state: any) => {
+    return {
+        basket: state.stores.basket,
+        selectedStore: state.stores.selectedStore
+    }
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+    return {
+
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SigninToolbar);
